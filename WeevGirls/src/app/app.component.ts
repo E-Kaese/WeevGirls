f63343ce7a233afc1b81fafdcb960cdcca306864
E-Kaese <ernstkaese@gmail.com>
2018-09-18 08:38:54 +0200

    Tried again to fix dependency vulnerability

--- WeevGirls/src/app/app.component.ts
+++ WeevGirls/src/app/app.component.ts
@@ -6,14 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  player: YT.Player;
   public id = 'c0bsKc4tiuY';
+  player: YT.Player;
 
   savePlayer(player) {
     this.player = player;
-    console.log('player instance', player);
+    // console.log('player instance', player);
   }
   onStateChange(event) {
-    console.log('player state', event.data);
+    // console.log('player state', event.data);
   }
 }
